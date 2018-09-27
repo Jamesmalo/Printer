ef5c3ae64971f178645aa462859f2d5b2b0c53d2
Jamesmalo <9jmalo@stpatrick.org>
2018-09-27 13:54:44 -0500

    Add files via upload

--- printer.c
+++ printer.c
@@ -13,17 +13,22 @@ struct Printer{
 
 };
 
-int trayStatus(tray*,printer*){
+int trayStatus(&T1,&trays){
   
   return 0;
 }
 
-int reloadTray(){
+int reloadTray(&trays,){
   return 1;
 }
 
-int printerStatus(){
+int printerStatus(&name){
+  for(int i=0;i<6;i++){ 
+    let total = total + PaperTray[i].quantity;
+  }
+  return total;
   return 0;
+
 }
 
 int print(){
@@ -32,13 +37,20 @@ int print(){
 
 int main(){
   //make users
-
+  User u1= {"uno",0};
+  User u2= {"two",1};
+  User u3= {"three",2};
 
   //make copyJobs
 
 
   //make trays (6 trays)
-
+  PaperTray T1  = {1,2000,2000};
+  PaperTray T2  = {2,2000,2000};
+  PaperTray T3  = {3,2000,2000};
+  PaperTray T4  = {4,2000,2000};
+  PaperTray T5  = {5,1000,1000};
+  PaperTray T6  = {6,1000,1000};
 
   //make printer
 
